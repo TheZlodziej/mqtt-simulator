d7988eef93f88ebca9be6931ea23a10d43c491f7
Jakub Wicher <galaxykuby@gmail.com>
2023-09-07 18:02:10 +0200

    main file works with both terminal/gui mode

--- main.py
+++ main.py
@@ -1,35 +1,73 @@
 from mqttsim import MqttSim, MqttSimConfig
 from argparse import ArgumentParser
-
+from logging import getLogger, Formatter, FileHandler, DEBUG, StreamHandler
+from PySide6.QtWidgets import QApplication
+from gui.ui import MqttSimMainWindow
+from sys import stdout, exit
+from time import sleep
 
 def main(args):
+    # config setup
     config = MqttSimConfig('config.json')
+    # end config setup
 
+    # standalone functions (cmd line config edit)
     if args.add_topic:
-        topic, format = args.add_topic
-        config.put_topic(topic, format)
+        topic, format, interval = args.add_topic
+        config.put_topic(topic, format, interval)
         return
 
     if args.set_broker:
         host, port = args.set_broker
         config.put_broker(host, int(port))
         return
+    # cmd line config edit
+    
+    # logger setup
+    logger = getLogger("MqttSimLogger")
+    logger.setLevel(DEBUG)
+    file_handler = FileHandler("logs.txt")
+    file_handler.setFormatter(
+        Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+    if args.verbose:
+        logger.addHandler(StreamHandler(stdout))
+    # end logger setup
 
-    app = MqttSim(config)
-    while True:
-        app.loop()
+    # sim setup
+    sim = MqttSim(config, logger)
+    sim.start()
+    if args.no_gui:
+        if not sim.connect_to_broker():
+            exit(1)
+        while True:
+            try:
+                sleep(0.1) # dummy task
+            except KeyboardInterrupt:
+                break
+    else:
+        app = QApplication()
+        window = MqttSimMainWindow(sim)
+        window.show()
+        app.exec() 
+    sim.stop()
+    # end sim setup
 
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser(prog='mqtt simulator',
                             description='simulate flow of data over mqtt')
+    parser.add_argument("-nogui", "--no-gui", action="store_true",
+                        help="Specify whether the app should launch without graphical user interface")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Print logs in console")
+
     parser.add_argument("-at",
                         "--add-topic",
-                        metavar=("topic_name", "data_format"),
+                        metavar=("topic_name", "data_format", "interval"),
                         type=str,
-                        nargs=2,
+                        nargs=3,
                         help="Add topic to config")
-
     parser.add_argument("-sb",
                         "--set-broker",
                         metavar=("host", "port"),
--- mqttsim.py
+++ mqttsim.py
@@ -1,5 +1,10 @@
 from confighandler import ConfigHandler
 from paho.mqtt.client import Client, CONNACK_ACCEPTED
+from threading import Thread
+from datetime import datetime
+from time import sleep
+from random import randint, random
+
 
 class MqttSimConfig:
     def __init__(self, path: str):
@@ -32,10 +37,41 @@ class MqttSimConfig:
 
 
 class MqttSim:
-    def __init__(self, config: MqttSimConfig, logger):
+    def __init__(self, config: MqttSimConfig, logger: any):
         self.__logger = logger
         self.__config = config
         self.__setup_client()
+        self.__setup_publishing_thread()
+
+    def __setup_publishing_thread(self) -> None:
+        self.__should_stop_publishing_thread = False
+        self.__publishing_thread = Thread(target=self.__publishing_thread_fn)
+
+    def __publishing_thread_fn(self) -> None:
+        def make_data(data_format) -> str:
+            return data_format.replace(r"<%randi%>", str(randint(-2**31, 2**31 - 1))).replace(r"<%randf%>", str(random())).replace(r"<%randu%>", str(randint(0, 2**32-1)))
+
+        def time_diff_in_seconds(time1, time2) -> int:
+            diff_dt = time1 - time2
+            return diff_dt.total_seconds()
+
+        topics = self.__config.get_topics()
+        last_sent = {topic: datetime.now() for topic in topics.keys()}
+        while not self.__should_stop_publishing_thread:
+            if not self.is_connected_to_broker():
+                sleep(0.5)
+                continue
+            now = datetime.now()
+            for topic, config in topics.items():
+                if topic not in last_sent:
+                    last_sent[topic] = now
+                    continue
+                if time_diff_in_seconds(now, last_sent[topic]) > config['interval']:
+                    self.__logger.info(f"Publishing data on {topic}")
+                    self.__client.publish(
+                        topic, make_data(config['data_format']))
+                    last_sent[topic] = now
+            sleep(0.01)
 
     def __setup_client(self) -> None:
         def on_message(client, userdata, message):
@@ -45,7 +81,8 @@ class MqttSim:
             if rc == CONNACK_ACCEPTED:
                 self.__logger.info("Connected to broker.")
             else:
-                self.__logger.error(f"Error when connecting to broker (rc={rc}).")
+                self.__logger.error(
+                    f"Error when connecting to broker (rc={rc}).")
 
         def on_disconnect(client, userdata, rc):
             if rc == 0:
@@ -58,42 +95,59 @@ class MqttSim:
         self.__client.on_connect = on_connect
         self.__client.on_disconnect = on_disconnect
 
+    # start publishing thread
+    def start(self) -> bool:
+        self.__publishing_thread.start()
+
+    # disconnect from broker, stop publishing thread and wait for it to join
+    def stop(self) -> None:
+        self.disconnect_from_broker()
+        self.__should_stop_publishing_thread = True
+        self.__publishing_thread.join()
+
     def is_connected_to_broker(self) -> bool:
         return self.__client.is_connected()
 
+    # connects to broker and starts client loop
     def connect_to_broker(self) -> bool:
         host, port = self.__config.get_broker()
         self.__logger.info(f"Trying to connect to broker {host}:{port}...")
         try:
-            self.__client.connect(host, port)
+            self.__client.connect(host, port)  # TODO: connect_async ?
             self.__client.loop_start()
         except ConnectionError:
             self.__logger.error(f"Coulnd't connect to broker {host}:{port}.")
             return False
         except Exception:
-            self.__logger.error(f"Unknown error occured when trying to connect to broker.")
+            self.__logger.error(
+                f"Unknown error occured when trying to connect to broker.")
             return False
         return True
-    
+
     def disconnect_from_broker(self) -> None:
         self.__client.disconnect()
         self.__client.loop_stop()
 
+    # Sets broker host and port (and saves it into config file)
     def set_broker(self, host: str, port: int) -> None:
-        curr_host, curr_port = self.__config.get_broker()
-        if host == curr_host and port == curr_port:
-            return None
         self.__config.put_broker(host, port)
         self.__logger.info(f"Changed broker to {host}:{port}.")
 
+    # Removes topic from config (and saves it into config file).
+    # If publishing thread was already started, it will take the topic into account.
     def remove_topic(self, topic: str) -> None:
         self.__client.unsubscribe(topic)
         self.__config.remove_topic(topic)
         self.__logger.info(f"Removed topic: {topic}.")
 
+    # Adds topic to config (and saves it into config file).
+    # If publishing thread was already started, it will take the topic into account.
     def add_topic(self, topic: str, topic_config: dict) -> None:
         self.__config.put_topic(topic, topic_config)
         self.__logger.info(f"Added topic: {topic}.")
+
+    def get_logger(self) -> any:
+        return self.__logger
 
     def get_config(self) -> MqttSimConfig:
         return self.__config
