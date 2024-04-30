afa34db2018181226b06a2dc333c2253348fbe96
minekuba <galaxykuby@gmail.com>
2024-04-30 19:05:43 +0200

    add: option to read format from file during topic creation

--- src/gui/generated/addtopicdialog.py
+++ src/gui/generated/addtopicdialog.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'addtopicdialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.2
+## Created by: Qt User Interface Compiler version 6.7.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -16,15 +16,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QCheckBox, QDialog,
-    QDialogButtonBox, QDoubleSpinBox, QFormLayout, QLabel,
-    QLineEdit, QPlainTextEdit, QSizePolicy, QVBoxLayout,
+    QDialogButtonBox, QDoubleSpinBox, QFormLayout, QHBoxLayout,
+    QLabel, QLayout, QLineEdit, QPlainTextEdit,
+    QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
     QWidget)
 
 class Ui_AddTopicDialog(object):
     def setupUi(self, AddTopicDialog):
         if not AddTopicDialog.objectName():
             AddTopicDialog.setObjectName(u"AddTopicDialog")
-        AddTopicDialog.resize(399, 251)
+        AddTopicDialog.resize(399, 280)
         font = QFont()
         font.setPointSize(12)
         AddTopicDialog.setFont(font)
@@ -38,54 +39,88 @@ class Ui_AddTopicDialog(object):
         font1.setPointSize(14)
         self.label.setFont(font1)
 
-        self.verticalLayout.addWidget(self.label, 0, Qt.AlignTop)
+        self.verticalLayout.addWidget(self.label, 0, Qt.AlignmentFlag.AlignTop)
 
         self.formLayout = QFormLayout()
         self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.ExpandingFieldsGrow)
+        self.formLayout.setVerticalSpacing(5)
         self.name_lbl = QLabel(AddTopicDialog)
         self.name_lbl.setObjectName(u"name_lbl")
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.name_lbl)
+
+        self.name_line_edit = QLineEdit(AddTopicDialog)
+        self.name_line_edit.setObjectName(u"name_line_edit")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.name_line_edit.sizePolicy().hasHeightForWidth())
+        self.name_line_edit.setSizePolicy(sizePolicy)
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.name_line_edit)
 
         self.format_lbl = QLabel(AddTopicDialog)
         self.format_lbl.setObjectName(u"format_lbl")
 
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.format_lbl)
 
+        self.format_vlayout = QVBoxLayout()
+        self.format_vlayout.setSpacing(0)
+        self.format_vlayout.setObjectName(u"format_vlayout")
+        self.format_text_edit = QPlainTextEdit(AddTopicDialog)
+        self.format_text_edit.setObjectName(u"format_text_edit")
+
+        self.format_vlayout.addWidget(self.format_text_edit)
+
+        self.format_hlayout = QHBoxLayout()
+        self.format_hlayout.setObjectName(u"format_hlayout")
+        self.format_hlayout.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
+        self.load_from_file_hspacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.format_hlayout.addItem(self.load_from_file_hspacer)
+
+        self.load_from_file_btn = QPushButton(AddTopicDialog)
+        self.load_from_file_btn.setObjectName(u"load_from_file_btn")
+        self.load_from_file_btn.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.format_hlayout.addWidget(self.load_from_file_btn)
+
+
+        self.format_vlayout.addLayout(self.format_hlayout)
+
+
+        self.formLayout.setLayout(1, QFormLayout.FieldRole, self.format_vlayout)
+
         self.interval_lbl = QLabel(AddTopicDialog)
         self.interval_lbl.setObjectName(u"interval_lbl")
 
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.interval_lbl)
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.interval_lbl)
 
         self.interval_spin_box = QDoubleSpinBox(AddTopicDialog)
         self.interval_spin_box.setObjectName(u"interval_spin_box")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.interval_spin_box.sizePolicy().hasHeightForWidth())
+        self.interval_spin_box.setSizePolicy(sizePolicy1)
         self.interval_spin_box.setMinimum(0.100000000000000)
         self.interval_spin_box.setMaximum(60.000000000000000)
         self.interval_spin_box.setSingleStep(0.500000000000000)
         self.interval_spin_box.setValue(1.500000000000000)
 
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.interval_spin_box)
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.interval_spin_box)
 
-        self.name_line_edit = QLineEdit(AddTopicDialog)
-        self.name_line_edit.setObjectName(u"name_line_edit")
+        self.manual_lbl = QLabel(AddTopicDialog)
+        self.manual_lbl.setObjectName(u"manual_lbl")
 
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.name_line_edit)
+        self.formLayout.setWidget(5, QFormLayout.LabelRole, self.manual_lbl)
 
         self.manual_check_box = QCheckBox(AddTopicDialog)
         self.manual_check_box.setObjectName(u"manual_check_box")
         self.manual_check_box.setChecked(False)
 
-        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.manual_check_box)
-
-        self.manual_lbl = QLabel(AddTopicDialog)
-        self.manual_lbl.setObjectName(u"manual_lbl")
-
-        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.manual_lbl)
-
-        self.format_text_edit = QPlainTextEdit(AddTopicDialog)
-        self.format_text_edit.setObjectName(u"format_text_edit")
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.format_text_edit)
+        self.formLayout.setWidget(5, QFormLayout.FieldRole, self.manual_check_box)
 
 
         self.verticalLayout.addLayout(self.formLayout)
@@ -95,8 +130,9 @@ class Ui_AddTopicDialog(object):
 
         self.buttonBox = QDialogButtonBox(AddTopicDialog)
         self.buttonBox.setObjectName(u"buttonBox")
-        self.buttonBox.setOrientation(Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox.setCursor(QCursor(Qt.PointingHandCursor))
+        self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
 
         self.verticalLayout_2.addWidget(self.buttonBox)
 
@@ -112,19 +148,20 @@ class Ui_AddTopicDialog(object):
         AddTopicDialog.setWindowTitle(QCoreApplication.translate("AddTopicDialog", u"Add topic", None))
         self.label.setText(QCoreApplication.translate("AddTopicDialog", u"Add topic", None))
         self.name_lbl.setText(QCoreApplication.translate("AddTopicDialog", u"Name", None))
+#if QT_CONFIG(tooltip)
+        self.name_line_edit.setToolTip(QCoreApplication.translate("AddTopicDialog", u"Name of topic", None))
+#endif // QT_CONFIG(tooltip)
         self.format_lbl.setText(QCoreApplication.translate("AddTopicDialog", u"Format", None))
+        self.format_text_edit.setPlainText(QCoreApplication.translate("AddTopicDialog", u"{ \"data\": <%randi%> }", None))
+        self.load_from_file_btn.setText(QCoreApplication.translate("AddTopicDialog", u"Load from file", None))
         self.interval_lbl.setText(QCoreApplication.translate("AddTopicDialog", u"Interval (seconds)", None))
 #if QT_CONFIG(tooltip)
         self.interval_spin_box.setToolTip(QCoreApplication.translate("AddTopicDialog", u"Interval of incoming data", None))
 #endif // QT_CONFIG(tooltip)
-#if QT_CONFIG(tooltip)
-        self.name_line_edit.setToolTip(QCoreApplication.translate("AddTopicDialog", u"Name of topic", None))
-#endif // QT_CONFIG(tooltip)
+        self.manual_lbl.setText(QCoreApplication.translate("AddTopicDialog", u"Manual", None))
 #if QT_CONFIG(tooltip)
         self.manual_check_box.setToolTip(QCoreApplication.translate("AddTopicDialog", u"If checked, the data will be automatically send every <interval> seconds", None))
 #endif // QT_CONFIG(tooltip)
         self.manual_check_box.setText("")
-        self.manual_lbl.setText(QCoreApplication.translate("AddTopicDialog", u"Manual", None))
-        self.format_text_edit.setPlainText(QCoreApplication.translate("AddTopicDialog", u"{ \"data\": <%randi%> }", None))
     # retranslateUi
 
--- src/gui/ui.py
+++ src/gui/ui.py
@@ -7,6 +7,7 @@ from PySide6.QtWidgets import (
     QLabel,
     QWidget,
     QMessageBox,
+    QFileDialog,
 )
 from PySide6.QtCore import Qt, QCoreApplication
 from PySide6.QtGui import QIcon
@@ -32,6 +33,18 @@ class MqttSimAddTopicWindow(Ui_AddTopicDialog, QDialog):
         super(MqttSimAddTopicWindow, self).__init__()
         self.setupUi(self)
         self.setWindowIcon(QIcon(":/icons/mqtt.svg"))
+        self.load_from_file_btn.clicked.connect(self.__on_load_from_file_btn_clicked)
+
+    def __on_load_from_file_btn_clicked(self) -> None:
+        filename, _ = QFileDialog.getOpenFileName(
+            self,
+            "Select file",
+            None,
+            "All files (*.*);;Text files (*.txt);;JSON files (*.json)",
+        )
+        print(filename)
+        with open(filename, "r") as file:
+            self.format_text_edit.setPlainText(file.read())
 
 
 class MqttSimEditTopicWindow(Ui_EditTopicDialog, QDialog):
@@ -69,7 +82,9 @@ class MqttSimTopicWidget(QWidget):
         hlayout.addWidget(self.remove_btn)
 
         # edit btn
-        self.edit_btn = MqttSimTopicToolButton(":/icons/edit.svg", QCoreApplication.translate("MainWindow", "Edit", None))
+        self.edit_btn = MqttSimTopicToolButton(
+            ":/icons/edit.svg", QCoreApplication.translate("MainWindow", "Edit", None)
+        )
         self.edit_btn.setCursor(Qt.CursorShape.PointingHandCursor)
         hlayout.addWidget(self.edit_btn)
 
@@ -104,12 +119,22 @@ class MqttSimMainWindow(Ui_MainWindow, QMainWindow):
         def on_broker_connect_btn_clicked() -> None:
             if self.__sim.is_connected_to_broker():
                 self.__sim.disconnect_from_broker()
-                self.broker_connect_btn.setText(QCoreApplication.translate("MainWindow", "Connect", None))
-                self.broker_connect_btn.setToolTip(QCoreApplication.translate("MainWindow", "Connect to broker", None))
+                self.broker_connect_btn.setText(
+                    QCoreApplication.translate("MainWindow", "Connect", None)
+                )
+                self.broker_connect_btn.setToolTip(
+                    QCoreApplication.translate("MainWindow", "Connect to broker", None)
+                )
             else:
                 if self.__sim.connect_to_broker():
-                    self.broker_connect_btn.setText(QCoreApplication.translate("MainWindow", "Disconnect", None))
-                    self.broker_connect_btn.setToolTip(QCoreApplication.translate("MainWindow", "Disconnect from broker", None))
+                    self.broker_connect_btn.setText(
+                        QCoreApplication.translate("MainWindow", "Disconnect", None)
+                    )
+                    self.broker_connect_btn.setToolTip(
+                        QCoreApplication.translate(
+                            "MainWindow", "Disconnect from broker", None
+                        )
+                    )
 
         def on_clear_logs_btn_clicked() -> None:
             self.logs_list.clear()
@@ -117,21 +142,28 @@ class MqttSimMainWindow(Ui_MainWindow, QMainWindow):
 
         def on_add_topic_btn_clicked() -> None:
             def validate_input(topic_name, topic_config) -> bool:
-                return len(topic_name) > 0 and topic_name not in self.__config.get_topics().keys()
+                return (
+                    len(topic_name) > 0
+                    and topic_name not in self.__config.get_topics().keys()
+                )
 
             add_topic_window = MqttSimAddTopicWindow()
-            if add_topic_window.exec():
-                topic_name = add_topic_window.name_line_edit.text()
-                topic_config = {
-                    "data_format": add_topic_window.format_text_edit.toPlainText(),
-                    "interval": add_topic_window.interval_spin_box.value(),
-                    "manual": add_topic_window.manual_check_box.isChecked(),
-                }
-                if validate_input(topic_name, topic_config):
-                    self.__sim.add_topic(topic_name, topic_config)
-                    self.__add_topic_to_item_list(topic_name)
+            while True:
+                if add_topic_window.exec() == QDialog.Accepted:
+                    topic_name = add_topic_window.name_line_edit.text()
+                    topic_config = {
+                        "data_format": add_topic_window.format_text_edit.toPlainText(),
+                        "interval": add_topic_window.interval_spin_box.value(),
+                        "manual": add_topic_window.manual_check_box.isChecked(),
+                    }
+                    if validate_input(topic_name, topic_config):
+                        self.__sim.add_topic(topic_name, topic_config)
+                        self.__add_topic_to_item_list(topic_name)
+                        break  # Break out of the loop if input is valid and accepted
+                    else:
+                        QMessageBox().critical(self, "Error!", "Invalid topic input.")
                 else:
-                    QMessageBox().critical(self, "Error!", "Invalid topic input.")
+                    break  # Break out of the loop if dialog is cancelled
 
         def on_broker_info_changed() -> None:
             self.__sim.set_broker(self.broker_hostname.text(), self.broker_port.value())
@@ -169,11 +201,15 @@ class MqttSimMainWindow(Ui_MainWindow, QMainWindow):
                 }
                 if edited_topic_data != topic_data:
                     self.__sim.edit(topic, edited_topic_data)
-                    self.__logger.info(f"Edited topic {topic} ({topic_data} -> {edited_topic_data}).")
+                    self.__logger.info(
+                        f"Edited topic {topic} ({topic_data} -> {edited_topic_data})."
+                    )
 
         topic_widget.remove_btn.clicked.connect(partial(on_remove_btn_clicked, topic))
         topic_widget.edit_btn.clicked.connect(partial(on_edit_btn_clicked, topic))
-        topic_widget.send_now_btn.clicked.connect(partial(self.__sim.send_single_message, topic))
+        topic_widget.send_now_btn.clicked.connect(
+            partial(self.__sim.send_single_message, topic)
+        )
 
         self.topics_list.insertWidget(0, topic_widget)
 
