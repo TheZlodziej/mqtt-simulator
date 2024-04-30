34c8a653153071a11cb0d8392f93c3ac2412dfc6
minekuba <galaxykuby@gmail.com>
2024-04-30 16:37:32 +0200

    fix: random min/max number regex; change: file can be now read without splitting text

--- src/mqttsimdatagenerator.py
+++ src/mqttsimdatagenerator.py
@@ -5,6 +5,7 @@ from uuid import uuid1 as uuid
 from functools import partial
 from datetime import datetime
 
+
 class MqttSimDataGenerator:
     def __init__(self, data_format: str):
         self.__make_formatted_string(data_format)
@@ -47,32 +48,43 @@ class MqttSimDataGenerator:
             function_mapper.get(function)(id, args)
             self.__format_str = self.__format_str.replace(to_replace, f"{{{id}}}", 1)
 
-    def __extract_min_max_or_default(self, args: str, dflt: (int | float, int | float), conv_fun: object) -> (int | float, int | float):
-        min_match = search(r"min=(-?\d*.\d+)", args)
-        max_match = search(r"min=(-?\d*.\d+)", args)
-
+    def __extract_min_max_or_default(
+        self, args: str, dflt: (int | float, int | float), conv_fun: object
+    ) -> (int | float, int | float):
+        min_match = search(r"min=(-?\d+(?:\.\d+)?)", args)
+        max_match = search(r"max=(-?\d+(?:\.\d+)?)", args)
         min_val = conv_fun(min_match.group(1)) if min_match is not None else dflt[0]
         max_val = conv_fun(max_match.group(1)) if max_match is not None else dflt[1]
 
         return (min_val, max_val)
 
-    def __extract_collection_length_or_none(self, args: str) -> (list | None, int | None):
-        collection_match = search(r'collection=\[([^\]])*\]', args)
+    def __extract_collection_length_or_none(
+        self, args: str
+    ) -> (list | None, int | None):
+        collection_match = search(r"collection=\[([^\]])*\]", args)
         length_match = search(r"length=(\d+)", args)
         if collection_match is not None:
             collection_value = collection_match.group(0)
-            collection_strings = findall(r'["\']([^"\']*)["\']', collection_value) if collection_value else []
+            collection_strings = (
+                findall(r'["\']([^"\']*)["\']', collection_value)
+                if collection_value
+                else []
+            )
             return (collection_strings, None)
 
         length_val = int(length_match.group(1)) if length_match is not None else None
         return (None, length_val)
 
-    def __extract_src_and_separator_or_default(self, args: str, dflt: (str | None, str)) -> (str | None, str):
+    def __extract_src_and_separator_or_default(
+        self, args: str, dflt: (str | None, str)
+    ) -> (str | None, str):
         src_match = search(r'src=["\'](.*?)["\']', args)
         separator_match = search(r'separator=["\'](.*?)["\']', args)
 
         src_value = src_match.group(1) if src_match is not None else dflt[0]
-        separator_value = separator_match.group(1) if separator_match is not None else dflt[1]
+        separator_value = (
+            separator_match.group(1) if separator_match is not None else dflt[1]
+        )
         return (src_value, separator_value)
 
     def __init_file(self, id: str, args: str) -> None:
@@ -80,12 +92,17 @@ class MqttSimDataGenerator:
             self.__file_data = dict()
 
         splitted_file_content = ["no file source"]
-        src_val, separator_val = self.__extract_src_and_separator_or_default(args, (None, "\n"))
+        src_val, separator_val = self.__extract_src_and_separator_or_default(
+            args, (None, None)
+        )
 
         if src_val is not None:
             try:
                 with open(src_val, "r") as file:
-                    splitted_file_content = file.read().split(separator_val)
+                    if separator_val is None:
+                        splitted_file_content = [file.read()]
+                    else:
+                        splitted_file_content = file.read().split(separator_val)
             except FileNotFoundError:
                 splitted_file_content = ["file not found"]
 
@@ -93,7 +110,9 @@ class MqttSimDataGenerator:
         self.__replace_dict[id] = partial(self.__next_file_value, id)
 
     def __init_randi(self, id: str, args: str) -> None:
-        min_val, max_val = self.__extract_min_max_or_default(args, (-(2**31), 2**31 - 1), int)
+        min_val, max_val = self.__extract_min_max_or_default(
+            args, (-(2**31), 2**31 - 1), int
+        )
         self.__replace_dict[id] = partial(self.__next_randi, min_val, max_val)
 
     def __init_randu(self, id: str, args: str) -> None:
@@ -101,7 +120,7 @@ class MqttSimDataGenerator:
         self.__replace_dict[id] = partial(self.__next_randu, min_val, max_val)
 
     def __init_randf(self, id: str, args: str) -> None:
-        min_val, max_val = self.__extract_min_max_or_default(args, (0, 1), float)
+        min_val, max_val = self.__extract_min_max_or_default(args, (0.0, 1.0), float)
         self.__replace_dict[id] = partial(self.__next_randf, min_val, max_val)
 
     def __init_rands(self, id: str, args: str) -> None:
@@ -169,7 +188,6 @@ class MqttSimDataGenerator:
         if collection is not None and collection:
             return f'"{choice(collection)}"'
         return f'"{"".join(choice(ascii_letters) for _ in range(length if length is not None else 10))}"'
-    
 
     # handle time
     # returns current time
